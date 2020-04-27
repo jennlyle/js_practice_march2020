7fdaa5a18511c50c51e4c0f91afcb41ec132f2ec
jennlyle <jennifer.g.lyle@gmail.com>
2020-04-27 10:13:20 +0100

    Cleaned up week 1 and ESLint week 3 cuz I forgot

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,50 +1,38 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
   return word.replace(word.charAt(0), word.charAt(0).toUpperCase());
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
   return firstName.charAt(0) + "." + lastName.charAt(0);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
   return Math.round((originalPrice * (vatRate / 100) + originalPrice) * 100) / 100
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
   return Math.round((originalPrice - (originalPrice * reduction/100)) * 100) / 100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
-  /* Wow!  I did like... 3 cycles of refactoring on this to make it work and it ran! */
-  /* It doesn't look too terribly readable to me tho! */
-  return (str.length/2 === Math.floor(str.length/2)) ? (str.charAt((str.length/2) - 1)) + (str.charAt(str.length/2)) : str.charAt(str.length/2);
+    return (str.length/2 === Math.floor(str.length/2)) ? (str.charAt((str.length/2) - 1)) + (str.charAt(str.length/2)) : str.charAt(str.length/2);
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-  /* I feel like I cheated on this one, but after refactoring, its all mine, yeah? */
   return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
-  /* I can't quite remember how to refactor For Loops effectively, but the below 
-    runs... and lookit, I re-used a prior function =D */
   let newArray = [];
   for (let i = 0; i < words.length; i++){
     newArray[i] = reverseWord(words[i]);
@@ -54,7 +42,6 @@ function reverseAllWords(words) {
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
   let count = 0;
   for (let i = 0; i < users.length; i++){
     if (users[i]["type"] === "Linux"){
@@ -66,7 +53,6 @@ function countLinuxUsers(users) {
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
   let myTotal = 0;
   for (let i = 0; i < scores.length; i++){
       myTotal += scores[i];
@@ -76,7 +62,6 @@ function getMeanScore(scores) {
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
   if ((n / 3 === Math.floor(n/3)) && (n / 5 === Math.floor(n/5))){
       return "fizzbuzz";
   }
--- challenges/week3.js
+++ challenges/week3.js
@@ -1,7 +1,7 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   let mySquare = [];
-  for (i = 0; i < Object.values(nums).length; i++){
+  for (let i = 0; i < Object.values(nums).length; i++){
     mySquare[i] = Object.values(nums)[i] * Object.values(nums)[i];
   }
   return mySquare;
@@ -9,8 +9,8 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  myNewWord = "";
-  for (i = 0; i < Object.values(words).length; i++){
+  let myNewWord = "";
+  for (let i = 0; i < Object.values(words).length; i++){
     if (i === 0){
       myNewWord += Object.values(words)[i];
     }
@@ -19,13 +19,13 @@ function camelCaseWords(words) {
     }
   }
   return myNewWord;
-};
+}
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   let count = 0;
-  for (i = 0; i < Object.values(people).length; i++){
-    for (j = 0; j < Object.values(people)[i]["subjects"].length; j++){
+  for (let i = 0; i < Object.values(people).length; i++){
+    for (let j = 0; j < Object.values(people)[i]["subjects"].length; j++){
       count++;
     }
   }
@@ -35,9 +35,9 @@ function getTotalSubjects(people) {
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  count = 0;
-  for (i = 0; i < Object.values(menu).length; i++){
-    for (j = 0; j < Object.values(menu)[i]["ingredients"].length; j++){
+  let count = 0;
+  for (let i = 0; i < Object.values(menu).length; i++){
+    for (let j = 0; j < Object.values(menu)[i]["ingredients"].length; j++){
       if (ingredient === Object.values(menu)[i]["ingredients"][j]){
         count++;
       }
@@ -54,10 +54,10 @@ function checkIngredients(menu, ingredient) {
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  myArrayOfNums = [];
-  count = 0;
-  for (i = 0; i < Object.values(arr1).length; i++) {
-    for (j = 0; j < Object.values(arr2).length; j++){
+  let myArrayOfNums = [];
+  let count = 0;
+  for (let i = 0; i < Object.values(arr1).length; i++) {
+    for (let j = 0; j < Object.values(arr2).length; j++){
       if (Object.values(arr1)[i] === Object.values(arr2)[j]){
         myArrayOfNums[count] = Object.values(arr1)[i];
         count++;
