439b7f904d5e1e75f27bd907fe32a8069495b873
jennlyle <jennifer.g.lyle@gmail.com>
2020-04-27 11:08:37 +0100

    Lunch break

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,22 +1,39 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let myArray = [];
+  nums.forEach(function(num, index) {
+    num < 1 ? myArray.push(num) : 0;
+  });
+  return myArray;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let myArray = []
+  names.forEach(function(name, index){
+    name.charAt(0) === char ? myArray.push(name) : 0;
+  });
+  return myArray;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let myArray = [];
+  words.forEach(function (word, index) {
+    word.includes('to ') ? myArray.push(word) : 0;
+
+  });
+  return myArray;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let myArray = [];
+  nums.forEach(function(num, index){
+    Number.isInteger(num) ? myArray.push(num) : 0;
+  });
+  return myArray;
 }
 
 function getCities(users) {
--- test/week4.test.js
+++ test/week4.test.js
@@ -67,7 +67,7 @@ describe("getIntegers", () => {
   });
 });
 
-describe("getCities", () => {
+describe.only("getCities", () => {
   test("returns an array of the cities of each user", () => {
     const users = [
       {
