49279f386e1de2478633163b8901649075c6894c
jennlyle <jennifer.g.lyle@gmail.com>
2020-04-28 11:06:22 +0100

    Carrying on later today

--- challenges/week3.js
+++ challenges/week3.js
@@ -12,46 +12,74 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
+  /* One way to write it */
+  // let myNewWord = "";
+  // for (let i = 0; i < Object.values(words).length; i++){
+  //   if (i === 0){
+  //     myNewWord += Object.values(words)[i];
+  //   }
+  //   else {
+  //     myNewWord += Object.values(words)[i].charAt(0).toUpperCase() + Object.values(words)[i].slice(1);
+  //   }
+  // }
+  // return myNewWord;
+
+  /*Another way to write it */
   let myNewWord = "";
-  for (let i = 0; i < Object.values(words).length; i++){
-    if (i === 0){
-      myNewWord += Object.values(words)[i];
-    }
-    else {
-      myNewWord += Object.values(words)[i].charAt(0).toUpperCase() + Object.values(words)[i].slice(1);
-    }
-  }
+  words.forEach(function(word, index) {
+    index != 0 ? word = word.charAt(0).toUpperCase() + word.slice(1) : 0;
+    myNewWord += word;
+  });
   return myNewWord;
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
+  /* One way to write it */
+  // let count = 0;
+  // for (let i = 0; i < Object.values(people).length; i++){
+  //   for (let j = 0; j < Object.values(people)[i]["subjects"].length; j++){
+  //     count++;
+  //   }
+  // }
+  // return count;
+
+  /* Another way to write it */
   let count = 0;
-  for (let i = 0; i < Object.values(people).length; i++){
-    for (let j = 0; j < Object.values(people)[i]["subjects"].length; j++){
-      count++;
-    }
-  }
+  people.forEach(function (person){
+    count += person.subjects.length;
+  });
   return count;
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  let count = 0;
-  for (let i = 0; i < Object.values(menu).length; i++){
-    for (let j = 0; j < Object.values(menu)[i]["ingredients"].length; j++){
-      if (ingredient === Object.values(menu)[i]["ingredients"][j]){
-        count++;
-      }
-    }
-  }
-  if (count > 0){
-    return true;
-  }
-  else {
-    return false;
-  }
+  /* One way to write it */
+  // let count = 0;
+  // for (let i = 0; i < Object.values(menu).length; i++){
+  //   for (let j = 0; j < Object.values(menu)[i]["ingredients"].length; j++){
+  //     if (ingredient === Object.values(menu)[i]["ingredients"][j]){
+  //       count++;
+  //     }
+  //   }
+  // }
+  // if (count > 0){
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
+
+  /* Another way to write it */
+
+  let result = false;
+  menu.forEach(function (meal) {
+    if (meal.ingredients.indexOf(ingredient) === 0) {
+      result = true;
+    }    
+  });
+  return result;
 }
 
 function duplicateNumbers(arr1, arr2) {
